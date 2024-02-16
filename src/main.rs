f012fb7b5d8df195280effd68f695b36abb0677c
Chiara Sabaini <chiara.sabaini@studenti.unitn.it>
2024-02-16 19:22:23 +0100

    feat(debug): bare minimum needed to see if it's working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use midgard::world_generator::{WorldGenerator, WorldGeneratorParameters};
+use robotics_lib::runner::Runner;
+use rust_eze_ai_artemisia::ArtemisIA;
+
 fn main() {
+    let world_size = 200;
+    let params = WorldGeneratorParameters {
+        seed: 15,                    // fixed seed
+        world_size,             // smaller world
+        amount_of_rivers: Some(1.2), // more rivers
+        amount_of_streets: None,     // disable streets
+        ..Default::default()         // the rest of the parameters keep their default value
+    };
+    let mut world_generator = WorldGenerator::new(params);
+
+    let artemis = Box::new(ArtemisIA::new(world_size));
+    let run = Runner::new(artemis, &mut world_generator);
+
+    match run {
+        Ok(mut robot) => {
+            for _ in 0..42 {
+                let _ = robot.game_tick();
+            }
+        }
+        Err(e) => println!("{:?}", e),
+    }
+
     println!("in the end, it all ends.");
 }
